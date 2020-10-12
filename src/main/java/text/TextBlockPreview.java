cf47cae9809763c361e44b7f5e058dd5129f8889
Michal Olszewski <michal.a.olszewski@allegro.pl>
2020-10-12 18:53:19 +0200

    text block example

---
+++ src/main/java/text/TextBlockPreview.java
@@ -0,0 +1,14 @@
+package text;
+
+public class TextBlockPreview {
+	public static void main(String[] args) {
+		String html = """
+				<html>
+				   <body>
+				        <p>Hello, world - with Text Blocks</p>
+				    </body>
+				</html>
+				""";
+		System.err.println(html);
+	}
+}
